f475059d66fdd870ca795cb9eea92500f4e1774b
Gus <Argodoroth@gmail.com>
2020-12-22 17:07:15 +0000

    Added login functionality and some auth middleware

--- routes/web.php
+++ routes/web.php
@@ -14,33 +14,24 @@ use App\Http\Controllers\GameController;
 */
 
 Route::get('/', function () {
-    return view('home');
+    return view('welcome');
 });
 
-Route::get('/home/{name}', function($name){
-    return "$name's homepage!!";
-});
 
-Route::get('/other',function(){
-    return "OTHER PAGE";
-});
-
-Route::redirect('/else','/other');
-
-Route::get('/random', function(){
-    return "RANDOM AKSDHJLAKDLAKSJDAKL";
-});
-
-Route::get('/games', 'App\Http\Controllers\GameController@index')->name('games.index');
+Route::get('/games', 'App\Http\Controllers\GameController@index')->name('games.index')->middleware('auth');
 
 //Passes data from route to gameData view
 Route::any('gameData/{data}', function($data){
-    return view('gameData', ['data'=>$data]);
+    return view('gameData', ['data'=>$data])->middleware('auth');
 });
 
 //Creates a route to make a new game and is then able to post to index page
-Route::post('games','App\Http\Controllers\GameController@store')->name('games.store');
-Route::get('games/create', 'App\Http\Controllers\GameController@create')->name('games.create');
+Route::post('games','App\Http\Controllers\GameController@store')->name('games.store')->middleware('auth');
+Route::get('games/create', 'App\Http\Controllers\GameController@create')->name('games.create')->middleware('auth');
 
-Route::get('games/{game}', 'App\Http\Controllers\GameController@show')->name('games.show');
-Route::delete('games/{id}','App\Http\Controllers\GameController@destroy')->name('games.destroy');
+Route::get('games/{game}', 'App\Http\Controllers\GameController@show')->name('games.show')->middleware('auth');
+Route::delete('games/{id}','App\Http\Controllers\GameController@destroy')->name('games.destroy')->middleware('auth');
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home')->middleware('auth');
